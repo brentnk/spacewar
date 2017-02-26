0493513648db863fb62d44e9bfb4af302a9b0eb2
Anthony <anthonyguitarman13@gmail.com>
2017-02-25 23:12:37 -0700

    cleaned up layout

--- imports/ui/components/game-nav/game-nav.js
+++ imports/ui/components/game-nav/game-nav.js
@@ -1,1 +1,20 @@
+import { Meteor } from 'meteor/meteor';
 import './game-nav.html'
+
+
+Template.gamenav.helpers({
+  links: [
+      {name: "building1", level: 0},
+      {name: "building5", level: 0},
+      {name: "building4", level: 0},
+      {name: "building2", level: 0},
+      {name: "building3", level: 0},
+      {name: "building3", level: 0},
+      {name: "building3", level: 0},
+      {name: "building3", level: 0},
+      {name: "building3", level: 0},
+      {name: "building3", level: 0},
+      {name: "building3", level: 0},
+      {name: "building3", level: 0}
+    ]
+});
