df131a674d09af19377d84559791f772d09b6bce
Brent Nkomo <brent@choozle.com>
2017-02-25 23:16:02 -0700

    remove links api;

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,7 +1,6 @@
 // Fill the DB with example data on startup
 
 import { Meteor } from 'meteor/meteor';
-import { Links } from '../../api/links/links.js';
 import { Galaxies } from '../../api/galaxies/galaxies.js';
 
 var makeGenericPlanet = function(planetNumber) {
@@ -13,33 +12,6 @@ var makeGenericPlanet = function(planetNumber) {
 }
 
 Meteor.startup(() => {
-  // if the Links collection is empty
-  if (Links.find().count() === 0) {
-    const data = [
-      {
-        title: 'Do the Tutorial',
-        url: 'https://www.meteor.com/try',
-        createdAt: new Date(),
-      },
-      {
-        title: 'Follow the Guide',
-        url: 'http://guide.meteor.com',
-        createdAt: new Date(),
-      },
-      {
-        title: 'Read the Docs',
-        url: 'https://docs.meteor.com',
-        createdAt: new Date(),
-      },
-      {
-        title: 'Discussions',
-        url: 'https://forums.meteor.com',
-        createdAt: new Date(),
-      },
-    ];
-
-    data.forEach(link => Links.insert(link));
-  }
 
   if (Galaxies.find().count() === 0) {
     const data = [
--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,7 +1,4 @@
 // Register your apis here
 
-import '../../api/links/methods.js';
-import '../../api/links/server/publications.js';
-
 import '../../api/galaxies/methods.js';
 import '../../api/galaxies/server/publications.js';
