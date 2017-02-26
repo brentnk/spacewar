4898155cfefe4c48b94ccd5185f704c83aca4e29
Brent Nkomo <brent@choozle.com>
2017-02-25 22:22:54 -0700

    info component example;

--- imports/ui/components/info/info.js
+++ imports/ui/components/info/info.js
@@ -2,31 +2,12 @@ import { Links } from '/imports/api/links/links.js';
 import { Meteor } from 'meteor/meteor';
 import './info.html';
 
-Template.info.onCreated(function () {
-  Meteor.subscribe('links.all');
-});
-
 Template.info.helpers({
-  links() {
-    return Links.find({});
-  },
-});
-
-Template.info.events({
-  'submit .info-link-add'(event) {
-    event.preventDefault();
-
-    const target = event.target;
-    const title = target.title;
-    const url = target.url;
-
-    Meteor.call('links.insert', title.value, url.value, (error) => {
-      if (error) {
-        alert(error.error);
-      } else {
-        title.value = '';
-        url.value = '';
-      }
-    });
-  },
+  links: [
+      {name: "building1", level: 0},
+      {name: "building5", level: 0},
+      {name: "building4", level: 0},
+      {name: "building2", level: 0},
+      {name: "building3", level: 0},
+    ]
 });
