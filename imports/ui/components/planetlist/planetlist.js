0307fd77778b1b85b6e9af302ff73a9574cde6dd
Brent Nkomo <brent@choozle.com>
2017-02-27 17:03:34 -0700

    use bulma css; update ui;

--- imports/ui/components/planetlist/planetlist.js
+++ imports/ui/components/planetlist/planetlist.js
@@ -7,6 +7,10 @@ Template.planetlist.onCreated(function () {
   Meteor.subscribe('planets.mine');
 });
 
+Template.planetlist.onRendered(function() {
+  Holder.run();
+});
+
 Template.planetlist.helpers({
   planets() {
     return Planets.find({});
@@ -14,5 +18,13 @@ Template.planetlist.helpers({
   my_planets() {
     return Planets.find({});
   },
+  amOwner(planetOwner) {
+    return Meteor.userId() === planetOwner;
+  },
 });
 
+Template.planetlist.events({
+  'click .planetclaim'(e) {
+    Meteor.call('planets.claimByPlayer',e.currentTarget.attributes["planetid"].value);
+  }
+});
--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -1,1 +1,6 @@
 import './body.html';
+import { Meteor } from 'meteor/meteor';
+
+Template.App_body.onRendered(function() {
+  Holder.run();
+});
